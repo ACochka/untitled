74b639970630a9b12dc100aedcc59b8606ed18b4
ACochka <bashckatovshura@yandex.ru>
2023-01-22 23:34:01 +0300

    fff

--- src/Task1_2.java
+++
@@ -1,9 +0,0 @@
-public class Task1_2 {
-    public static void main(String[] args) {
-        System.out.println(triArea(7, 4));
-    }
-
-    public static float triArea(int firstDigit, int secondDigit) {
-        return (firstDigit*secondDigit)/2;
-    }
-}
--- src/Task1_8.java
+++
@@ -1,9 +0,0 @@
-public class Task1_8 {
-    public static void main(String[] args) {
-        System.out.println(nextEdge(9, 2));
-    }
-
-    public static int nextEdge(int firstSide, int secondSide ) {
-        return firstSide + secondSide - 1 ;
-    }
-}
